f63c23651bfa0e4a00709ca8f3eaaabcf26589c5
Alexander D. <maniac@pzskc383.net>
2026-01-18 01:48:59 +0200

    relocation+encoding

--- variants/nrf52840/t-echo/nicheGraphics.h
+++ variants/nrf52840/t-echo/nicheGraphics.h
@@ -58,9 +58,9 @@ void setupNicheGraphics()
     inkhud->setDisplayResilience(20, 1.5);
 
     // Select fonts
-    InkHUD::Applet::fontLarge = FREESANS_12PT_WIN1252;
-    InkHUD::Applet::fontMedium = FREESANS_9PT_WIN1252;
-    InkHUD::Applet::fontSmall = FREESANS_6PT_WIN1252;
+    InkHUD::Applet::fontLarge = FREESANS_12PT_WIN1251;
+    InkHUD::Applet::fontMedium = FREESANS_9PT_WIN1251;
+    InkHUD::Applet::fontSmall = FREESANS_6PT_WIN1251;
 
     // Customize default settings
     inkhud->persistence->settings.userTiles.maxCount = 2;              // Two applets side-by-side
